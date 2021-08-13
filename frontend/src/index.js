1e442ef28bb101b944310c35b9f86b926b683504
akshay2408 <akshaybabar2408@gmail.com>
2021-08-13 18:18:38 +0530

    Install component library and setup redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import createLogger from 'redux-logger';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+
+import reducers from './reducers';
+import App from './App';
+
+import './style/index.css';
+import 'mdbreact/dist/css/mdb.css';
+
+/*
+----------------------
+  Create Redux Store
+----------------------
+*/
+const store = createStore(
+  combineReducers(reducers),
+  applyMiddleware(thunk, createLogger)
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
